b951c73d6373dbe1925e2454c921864236617f04
develckm86 <develckm86@gmail.com>
2025-01-21 13:02:58 +0900

    수정 삭제 구현

--- src/com/tj703/employees/swing/DeptTable.java
+++ src/com/tj703/employees/swing/DeptTable.java
@@ -6,8 +6,8 @@ import com.tj703.employees.dto.DepartmentsDto;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.List;
 
 public class DeptTable extends JFrame {
@@ -34,7 +34,7 @@ public class DeptTable extends JFrame {
         });
         btnP.add(refreshBtn);
         addBtn.addActionListener((e)->{
-            new DeptAddDialog();
+            new DeptCrudDialog();
         });
         btnP.add(addBtn);
         this.add(btnP, BorderLayout.SOUTH);
@@ -69,6 +69,21 @@ public class DeptTable extends JFrame {
         //기존에 추가된 panel 을 삭제해야 다음 panel 을 추가할 수 있다.
         if(panel!=null) this.remove(panel);
         table = new JTable(data,columns);
+        table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //Object target=e.getSource(); //==target
+                //JTable table=(JTable)target; //캐스팅형변환
+                int row=table.getSelectedRow();
+                //int column=table.getSelectedColumn();
+                String deptNo=table.getValueAt(row,0).toString();
+                String deptName=table.getValueAt(row,1).toString();
+                System.out.println(deptNo+","+deptName);
+                //new DeptCrudDialog(); //등록 dialog
+                new DeptCrudDialog(deptNo,deptName); //수정 삭제 dialog
+            }
+        });
+
         scrollPane = new JScrollPane(table);
         panel = new JPanel();
         panel.add(scrollPane);
@@ -77,14 +92,19 @@ public class DeptTable extends JFrame {
         this.revalidate();
         this.repaint();
     }
-    class DeptAddDialog extends JDialog{
+    class DeptCrudDialog extends JDialog{
         private JTextField txtDeptNo; //==input
         private JTextField txtDeptName;
         private JLabel lbDeptNo;
         private JLabel lbDeptName;
         private JButton btnAdd;
         private JPanel panel;
-        public DeptAddDialog(){
+
+        private JButton updateBtn;
+        private JButton deleteBtn;
+        private JPanel btnP;
+
+        public DeptCrudDialog(){
             super(DeptTable.this,"부서등록 Dialog");
             setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             setBounds(1500, 100, 450, 300);
@@ -103,8 +123,8 @@ public class DeptTable extends JFrame {
                     if(create>0){
                         System.out.println("등록성공");
                         DeptTable.this.refreshTable();
-                        DeptAddDialog.this.setVisible(false); //Dialog
-                        DeptAddDialog.this.dispose();
+                        DeptCrudDialog.this.setVisible(false); //Dialog
+                        DeptCrudDialog.this.dispose();
                     }
                 } catch (Exception ex) {
                     ex.printStackTrace();
@@ -120,6 +140,68 @@ public class DeptTable extends JFrame {
             this.add(btnAdd,BorderLayout.SOUTH);
             setVisible(true);
         }
+        public DeptCrudDialog(String deptNo, String deptName){
+            super(DeptTable.this,"부서등록 Dialog");
+            setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            setBounds(1500, 100, 450, 300);
+            lbDeptNo=new JLabel("부서번호");
+            lbDeptName=new JLabel("부서이름");
+
+            txtDeptNo=new JTextField();
+            txtDeptNo.setText(deptNo);
+
+            txtDeptName=new JTextField();
+            txtDeptName.setText(deptName);
+
+            deleteBtn=new JButton("부서삭제");
+            updateBtn=new JButton("부서수정");
+            deleteBtn.addActionListener((e)->{
+                try{
+                    DepartmentsDao dao=new DepartmentsDaoImp();
+                    int delete=dao.delete(deptNo);
+                    if(delete>0){
+                        //this.setVisible(false);
+                        DeptCrudDialog.this.setVisible(false);
+                        DeptTable.this.refreshTable();
+                        DeptCrudDialog.this.dispose();
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+            updateBtn.addActionListener((e)->{
+                try{
+                    DepartmentsDto dto=new DepartmentsDto();
+                    dto.setDeptNo(deptNo);
+                    dto.setDeptName(txtDeptName.getText());
+                    DepartmentsDao dao=new DepartmentsDaoImp();
+                    int update=dao.update(dto);
+                    if(update>0){
+                        DeptCrudDialog.this.setVisible(false);
+                        DeptTable.this.refreshTable();
+                        DeptCrudDialog.this.dispose();
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+            btnP=new JPanel();
+            btnP.add(deleteBtn);
+            btnP.add(updateBtn);
+
+            panel=new JPanel();
+            panel.setLayout(new GridLayout(2,2));
+            panel.add(lbDeptNo);
+            panel.add(txtDeptNo);
+            panel.add(lbDeptName);
+            panel.add(txtDeptName);
+
+            this.add(panel);
+            this.add(btnP,BorderLayout.SOUTH);
+            setVisible(true);
+        }
+
+
     }
 
 
